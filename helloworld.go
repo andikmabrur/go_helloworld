8d6ebc387ab49432f08354088f9586a60454ddc7
andikmabrur <andikmabrur@gmail.com>
2018-11-21 14:02:00 +0700

    Add for

--- helloworld.go
+++ helloworld.go
@@ -5,4 +5,14 @@ import "fmt"
 func main() {
 	fmt.Println("Hello world!!!")
 	fmt.Println("I am Coming here")
+
+	i := 0
+	for {
+		i++
+		if i == 10 {
+			break
+		}
+
+		println("Print line : ", i)
+	}
 }
